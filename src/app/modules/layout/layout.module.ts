975a9725e39c7c61da6c16b50142fa43fad315b1
cemozbey <48527545+cemozbeyy@users.noreply.github.com>
2024-02-09 19:53:47 +0300

    components added folder structure almost done.

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { LayoutComponent } from './layout.component';
 import { LayoutRoutingModule } from './layout-routing.module';
+import { HeaderComponent, TableComponent } from './components';
+
 
 const COMPONENTS = [
     LayoutComponent,
+    HeaderComponent,
+    TableComponent
 ];
 
 @NgModule({
